f4ed0a783a3683d42da6c2be4d166a367d839854
Miguel Machado <mmachado@sourcedevelopers.com>
2022-03-26 12:46:45 +0100

    First form working version

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,14 +7,26 @@ const mapRecipes = (recipes) => {
     )
 }
 
-export const Recipe = ({ name, ingredients }) => {
+export const Recipe = ({ recipe }) => {
     return (
         <>
             <ul className='list-group'>
                 <li className='list-group-item'>
-                    <h4>{name}</h4>
+                    <h4>{recipe.title}</h4>
+                    <img src={recipe.image} alt={recipe.title} width="200" height="200"></img>
+                    <h5>Ratings:</h5>
+                    <p>{recipe.ratings}</p>
+                    <h5>Cook Time:</h5>
+                    <p>{recipe.cook_time}</p>
+                    <h5>Preparation Time:</h5>
+                    <p>{recipe.prep_time}</p>
+                    <h5>Category:</h5>
+                    <p>{recipe.category}</p>
+                    <h5>Author:</h5>
+                    <p>{recipe.author}</p>
+                    <h5>Ingredients:</h5>
                     <ul className='list-group'>
-                        {mapRecipes(ingredients)}
+                        {mapRecipes(recipe.ingredients)}
                     </ul>
                 </li>
             </ul>
